9ebbb697cec77bd1907ed2000632f98cccb7509d
rajudhoni <rajsekharoyal@gmail.com>
2020-09-06 07:29:06 +0530

    Updates

---
+++ precache-manifest.e2f4253ed81dd0e461f0c65a6558375b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d19b66709205281b757f80a666848be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f52187550badc7ca1e8",
+    "url": "/monsters-rolodex/static/css/main.3ac4bff6.chunk.css"
+  },
+  {
+    "revision": "1e1263d69cfebaf94b42",
+    "url": "/monsters-rolodex/static/js/2.407ee5d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.407ee5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f52187550badc7ca1e8",
+    "url": "/monsters-rolodex/static/js/main.1b7c4599.chunk.js"
+  },
+  {
+    "revision": "d2e3d63df8746766dfb1",
+    "url": "/monsters-rolodex/static/js/runtime-main.e4015b16.js"
+  }
+]);
